7bd5e55dbd78fcba48107de0d84bf01474799c9a
DELL <DELL@HIDALGOLAPTOP>
2023-10-16 15:40:12 -0600

    Agregar archivos de proyecto.

---
+++ ciudadInfo.API/CiudadesDataStore.cs
@@ -0,0 +1,35 @@
+﻿using ciudadInfo.API.Models;
+
+namespace ciudadInfo.API
+{
+    public class CiudadesDataStore
+    {
+        public List<CiudadDto> Ciudades { get; set; }
+
+        public CiudadesDataStore()
+        {
+            Ciudades = new List<CiudadDto>()
+            {
+                new CiudadDto()
+            {
+                Id = 1,
+                Nombre = "Estelí",
+                Descripcion = "Departamento principal del norte de Nicaragua"
+            },
+            new CiudadDto()
+            {
+                Id = 2,
+                Nombre = "Somoto",
+                Descripcion = "Ciudad con gran tradición cultural del norte de Nicaragua"
+            },
+            new CiudadDto()
+            {
+                Id = 3,
+                Nombre = "Jinotega",
+                Descripcion = "Ciudad de montañas del centro de Nicaragua"
+            }
+        }
+            
+        }
+    }
+}
---
+++ ciudadInfo.API/Controllers/CiudadesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ciudadInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/ciudades")]
+    public class CiudadesController:ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetCiudades()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, nombre = "Estelí"},
+                    new {id = 2, nombre = "Ocotal"}
+                });
+        }
+    }
+}
