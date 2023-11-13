8ed2bb6ccf64d4780460b583f6d602e1ad0f1dca
Augusto Hidalgo <augus.hidalgo@gmail.com>
2023-11-13 17:11:23 -0600

    Agregada la clase PuntoDeInteresParaCreacionDto.cs y la acción CrearPuntoDeInteres para procesar la petición POST. Agregados los Data Annotations para validar campos en la Dto.

--- ciudadInfo.API/Controllers/PuntosDeInteresController.cs
+++ ciudadInfo.API/Controllers/PuntosDeInteresController.cs
@@ -19,7 +19,7 @@ namespace ciudadInfo.API.Controllers
             return Ok(ciudad.PuntosDeInteres);
         }
 
-        [HttpGet("{puntodeinteresid}")]
+        [HttpGet("{puntodeinteresid}", Name = "GetPuntoDeInteres")]
         public ActionResult<PuntoDeInteresDto> GetPuntoDeInteres(
             int ciudadId, int puntoDeInteresID)
         {
@@ -36,6 +36,38 @@ namespace ciudadInfo.API.Controllers
                 return NotFound();
             }   
             return Ok(puntoDeInteres);
+        }
+        [HttpPost]
+        public ActionResult<PuntoDeInteresDto> CrearPuntoDeInteres(
+            int ciudadId,
+            PuntoDeInteresParaCreacionDto puntoDeInteres)
+        {
+            var ciudad = CiudadesDataStore.Actual.Ciudades.FirstOrDefault(c => c.Id == ciudadId);
+            if (ciudad == null)
+            {
+                return NotFound();
+            }
+
+            // para improvisar por ahora
+            var maxPuntoDeInteresId = CiudadesDataStore.Actual.Ciudades.SelectMany(
+                c => c.PuntosDeInteres).Max(p => p.Id);
+
+            var finalPuntoDeInteres = new PuntoDeInteresDto()
+            {
+                Id = ++maxPuntoDeInteresId,
+                Nombre = puntoDeInteres.Nombre,
+                Descripcion = puntoDeInteres.Descripcion
+            };
+
+            ciudad.PuntosDeInteres.Add(finalPuntoDeInteres);
+            
+            return CreatedAtRoute("GetPuntoDeInteres",
+                new
+                {
+                    ciudadId = ciudadId,
+                    puntoDeInteresId = finalPuntoDeInteres.Id
+                },
+                finalPuntoDeInteres);
         }
     }
 }
---
+++ ciudadInfo.API/Models/PuntoDeInteresParaCreacionDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ciudadInfo.API.Models
+{
+    public class PuntoDeInteresParaCreacionDto
+    {
+        [Required(ErrorMessage ="Debe incluir el Nombre")]
+        [MaxLength(20)]
+        public string Nombre { get; set; }=string.Empty;
+        [MaxLength(200)]
+        public string? Descripcion { get; set; }
+    }
+}
